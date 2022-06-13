2fb51d44efbeb3c3fdfca7ccc0c7f90373be6912
shubhambakade <ShubhamBakade25@gmail.com>
2022-06-13 10:22:41 +0530

    PHONE-8 :Create phoneBook Repository

---
+++ src/main/java/com/BikkadIT/PhoneBookApplication/repository/PhoneBookRepository.java
@@ -0,0 +1,7 @@
+package com.BikkadIT.PhoneBookApplication.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface PhoneBookRepository extends JpaRepository<T, ID> {
+
+}
