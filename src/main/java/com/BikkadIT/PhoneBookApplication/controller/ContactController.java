7e9f76e2a059b0c102b3bedff3dbad5f878dffb4
shubhambakade <ShubhamBakade25@gmail.com>
2022-06-16 23:04:11 +0530

    PHONE-15 :provide implementation for controller layer method for getting all data

--- src/main/java/com/BikkadIT/PhoneBookApplication/controller/ContactController.java
+++ src/main/java/com/BikkadIT/PhoneBookApplication/controller/ContactController.java
@@ -1,8 +1,11 @@
 package com.BikkadIT.PhoneBookApplication.controller;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.ResponseBody;
@@ -29,7 +32,21 @@ public class ContactController {
 	} else {
          String msg ="Contact Not Saved Successfully";
 		return new ResponseEntity<String>(msg,HttpStatus.BAD_REQUEST);
-	}
+	    }
    }
+	
+	@GetMapping(value="/getAllContact",produces = "APPLICATION/JSON")
+	public ResponseEntity<List<Contact>> getAllContact(){
+	
+		List<Contact> list = contactServiceImpl.getAllContact();
+		
+		if(list !=null) {
+			return new ResponseEntity<List<Contact>> (list,HttpStatus.OK);
+		}else
+		{
+			String msg="Data Not found";
+		return new ResponseEntity(msg,HttpStatus.BAD_REQUEST);
+		}
+	}
 		
 }
